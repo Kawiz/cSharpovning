57cc7c9beac2f06a908f1b53a50072d9cce56513
Kawiz <92087044+Kawiz@users.noreply.github.com>
2022-09-02 10:47:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = "Live and sleep!"
+            Console.WriteLine(message);
+        }
+    }
+}
